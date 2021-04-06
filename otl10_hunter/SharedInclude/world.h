2740265423311445926cc8bacb0b44dee5781b0f
Mikhail <oborotoffm@yandex.ru>
2021-04-07 02:43:12 +0300

    nothing

--- otl10_hunter/SharedInclude/world.h
+++ otl10_hunter/SharedInclude/world.h
@@ -39,15 +39,4 @@ struct World
             it.second.update(dt);
         }
     }
-
-    // Set new target position
-    void new_target() {
-        unsigned int seed = std::chrono::steady_clock::now().time_since_epoch().count(); // Random seed
-        std::default_random_engine generator(seed); // Generator
-        std::uniform_real_distribution<float> distribution_x(float(World::Size.x) * 0.05, float(World::Size.x) * 0.95); // Creating x distribution
-        std::uniform_real_distribution<float> distribution_y(float(World::Size.y) * 0.05, float(World::Size.y) * 0.95); // Creating y distribution
-
-        target.pos.x = distribution_x(generator); // Generating new x target pos
-        target.pos.y = distribution_y(generator); // Generating new y target pos
-    }
 };
