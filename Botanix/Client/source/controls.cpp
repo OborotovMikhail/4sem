ae354d848690902e99c85ce91783b4c5067ca1e1
Mikhail <oborotoffm@yandex.ru>
2021-04-27 00:33:15 +0300

    reworked controls on client

--- Botanix/Client/source/controls.cpp
+++ Botanix/Client/source/controls.cpp
@@ -4,142 +4,20 @@
 
 sf::Vector2f controls_arrows()
 {
-	sf::Vector2f v;
+    sf::Vector2f controls;
 
-    // zero movement
-    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Right) == sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        && (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) == sf::Keyboard::isKeyPressed(sf::Keyboard::Down)))
-    {
-        v.x = 0.0f;
-        v.y = 0.0f;
-    }
-    // left movement
-    if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && sf::Keyboard::isKeyPressed(sf::Keyboard::Left)
-        && (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) == sf::Keyboard::isKeyPressed(sf::Keyboard::Down)))
-    {
-        v.x = - 1.0f;
-        v.y = 0.0f;
-    }
-    // right movement
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && !sf::Keyboard::isKeyPressed(sf::Keyboard::Left)
-        && (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) == sf::Keyboard::isKeyPressed(sf::Keyboard::Down)))
-    {
-        v.x = 1.0f;
-        v.y = 0.0f;
-    }
-    // down movement
-    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Right) == sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        && !sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-    {
-        v.x = 0.0f;
-        v.y = 1.0f;
-    }
-    // up movement
-    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::Right) == sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-        && sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && !sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-    {
-        v.x = 0;
-        v.y = - 1.0f;
-    }
-    // left-down movement
-    if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && sf::Keyboard::isKeyPressed(sf::Keyboard::Left)
-        && !sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-    {
-        v.x = - 1.0f;
-        v.y = 1.0f;
-    }
-    // left-up movement
-    if (!sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && sf::Keyboard::isKeyPressed(sf::Keyboard::Left)
-        && sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && !sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-    {
-        v.x = - 1.0f;
-        v.y = - 1.0f;
-    }
-    // right-down movement
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && !sf::Keyboard::isKeyPressed(sf::Keyboard::Left)
-        && !sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-    {
-        v.x = 1.0f;
-        v.y = 1.0f;
-    }
-    // right-up movement
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && !sf::Keyboard::isKeyPressed(sf::Keyboard::Left)
-        && sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && !sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-    {
-        v.x = 1.0f;
-        v.y = - 1.0f;
-    }
-    
-    return v;
+    controls.x = float(sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) - float(sf::Keyboard::isKeyPressed(sf::Keyboard::Left));
+    controls.y = float(sf::Keyboard::isKeyPressed(sf::Keyboard::Down)) - float(sf::Keyboard::isKeyPressed(sf::Keyboard::Up));
+
+    return controls;
 }
 
 sf::Vector2f controls_wasd()
 {
-    sf::Vector2f v;
+    sf::Vector2f controls;
 
-    // zero movement
-    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::D) == sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-        && (sf::Keyboard::isKeyPressed(sf::Keyboard::W) == sf::Keyboard::isKeyPressed(sf::Keyboard::S)))
-    {
-        v.x = 0.0f;
-        v.y = 0.0f;
-    }
-    // left movement
-    if (!sf::Keyboard::isKeyPressed(sf::Keyboard::D) && sf::Keyboard::isKeyPressed(sf::Keyboard::A)
-        && (sf::Keyboard::isKeyPressed(sf::Keyboard::W) == sf::Keyboard::isKeyPressed(sf::Keyboard::S)))
-    {
-        v.x = - 1.0f;
-        v.y = 0.0f;
-    }
-    // right movement
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && !sf::Keyboard::isKeyPressed(sf::Keyboard::A)
-        && (sf::Keyboard::isKeyPressed(sf::Keyboard::W) == sf::Keyboard::isKeyPressed(sf::Keyboard::S)))
-    {
-        v.x = 1.0f;
-        v.y = 0.0f;
-    }
-    // down movement
-    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::D) == sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-        && !sf::Keyboard::isKeyPressed(sf::Keyboard::W) && sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-    {
-        v.x = 0.0f;
-        v.y = 1.0f;
-    }
-    // up movement
-    if ((sf::Keyboard::isKeyPressed(sf::Keyboard::D) == sf::Keyboard::isKeyPressed(sf::Keyboard::A))
-        && sf::Keyboard::isKeyPressed(sf::Keyboard::W) && !sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-    {
-        v.x = 0.0f;
-        v.y = - 1.0f;
-    }
-    // left-down movement
-    if (!sf::Keyboard::isKeyPressed(sf::Keyboard::D) && sf::Keyboard::isKeyPressed(sf::Keyboard::A)
-        && !sf::Keyboard::isKeyPressed(sf::Keyboard::W) && sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-    {
-        v.x = - 1.0f;
-        v.y = 1.0f;
-    }
-    // left-up movement
-    if (!sf::Keyboard::isKeyPressed(sf::Keyboard::D) && sf::Keyboard::isKeyPressed(sf::Keyboard::A)
-        && sf::Keyboard::isKeyPressed(sf::Keyboard::W) && !sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-    {
-        v.x = - 1.0f;
-        v.y = - 1.0f;
-    }
-    // right-down movement
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && !sf::Keyboard::isKeyPressed(sf::Keyboard::A)
-        && !sf::Keyboard::isKeyPressed(sf::Keyboard::W) && sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-    {
-        v.x = 1.0f;
-        v.y = 1.0f;
-    }
-    // right-up movement
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::D) && !sf::Keyboard::isKeyPressed(sf::Keyboard::A)
-        && sf::Keyboard::isKeyPressed(sf::Keyboard::W) && !sf::Keyboard::isKeyPressed(sf::Keyboard::S))
-    {
-        v.x = 1.0f;
-        v.y = - 1.0f;
-    }
+    controls.x = float(sf::Keyboard::isKeyPressed(sf::Keyboard::D)) - float(sf::Keyboard::isKeyPressed(sf::Keyboard::A));
+    controls.y = float(sf::Keyboard::isKeyPressed(sf::Keyboard::S)) - float(sf::Keyboard::isKeyPressed(sf::Keyboard::W));
 
-    return v;
+    return controls;
 }
